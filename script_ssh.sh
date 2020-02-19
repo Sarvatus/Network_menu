25d4b7518723fed3288626052c07065b7beb3456
root <root@ctom72.lab.net>
2020-02-19 10:37:59 +0100

    First steps... what exists?

--- script_ssh.sh
+++ script_ssh.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 echo "Echo"
+
+DIRECTORY=/.ssh
+#echo "$DIRECTORY AAAA"
+
+if [ -d "$DIRECTORY" ]; then
+echo "DIRECTORY $DIRECTORY EXISTS"
+
+else
+$(mkdir $DIRECTORY)
+fi
+
+FILE=/.ssh/authorized_keys
+if [ -f "$FILE" ]; then 
+
+echo "File $FILE EXISTS"
+
+else 
+$(touch "$DIRECTORY"/authorized_keys)
+$(chmod 600 /.ssh/authorized_keys)
+
+fi
