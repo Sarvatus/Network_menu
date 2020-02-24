c262c1690e8b677bcdb75733de7c7f4a4d9e5f88
root <root@ctom72.lab.net>
2020-02-24 14:57:20 +0100

    Huge changes - new look with menu

--- script_ssh.sh
+++ script_ssh.sh
@@ -1,67 +1,93 @@
 #!/bin/bash
 
-# Check if directory /.ssh exists 
+menu () {
+clear
+echo "####### SSHTOM #######"
+echo " "
+echo "1. Check existing of ~/.ssh directory"
+echo "2. Create ssh public and private keys and copy them to Your remote server"
+echo "3. Try Your remote connection"
+echo " "
+echo "Write exit to leave menu"
 
-DIRECTORY="/.ssh"
+read -p "Option: " option
+
+case "$option" in
+
+"1") clear ; check_existing ;;
+"2") clear ; generating ;;
+"3") clear ; remote ;; 
+"exit") clear ; echo "Bye" ; sleep1; clear ;;
+*)  clear ; echo "Write correct option!"
+esac
+}
+
+
+
+# Check  directory /.ssh exists 
+
+DIRECTORY="/root/.ssh"
 check_existing() {
-echo "Checking if $DIRECTORY  exists..."
+echo "Checking existing of $DIRECTORY  "
+echo " "
 if [ -d "$DIRECTORY" ]; then
-echo "DIRECTORY $DIRECTORY EXISTS"
+echo "DIRECTORY $DIRECTORY":" EXISTS!"
+echo "Check out directory ~/.ssh  and comeback to script!"
+ 
 
 else
 echo "Creating $DIRECTORY and changing permission to 700"
 $(mkdir $DIRECTORY)
 $(chmod 700 $DIRECTORY)
 fi
+
+echo " "
+echo "1. Back to menu"
+read -p "Option:  " back
+case $back in 
+"1") menu ;;
+esac 
 }
 
+generating () {
+echo "Generating new pair of RSA keys..."
+read -p "Write name of private key _(SERVER IP)" FILE_NAME
+if [[  -f /root/.ssh/"$FILE_NAME" ]] || [[  -f /root/.ssh/"$FILE_NAME"".pub" ]]  ;then 
+echo "$FILE_NAME"".pub OR $FILE_NAME exists"
 
-#FILE=/.ssh/authorized_keys
-#
-#echo "Checking if $FILE exists..."
-#if [ -f "$FILE" ]; then 
-#
-#echo "File $FILE EXISTS"
-#
-#else 
-#echo "Creating $FILE and changing permission to 600"
-#$(touch $FILE)
-#$(chmod 600 /.ssh/authorized_keys)
-#
-#fi
-#
-#if [ $(stat -c %a /.ssh/authorized_keys) -eq "600" ]; then
-#
-#echo "file have permision 600"
-#
-#else
-#echo "file have wrong permission"
-#echo "Changing permissions to 600..."
-#$(chmod 600 /.ssh/authorized_keys)
-#fi
-#
-#echo "Generating new pair of RSA keys..."
-#FILE_NAME="my.key"
-#echo "$FILE_NAME"".pub"
-#if [[  -f /.ssh/"$FILE_NAME" ]] || [[  -f /.ssh/"$FILE_NAME"".pub" ]]  ;then 
-#echo "$FILE_NAME"".pub OR $FILE_NAME exists"
-#fi
-#if [[ ! -f /.ssh/"$FILE_NAME" ]] &&  [[ !  -f /.ssh/"$FILE_NAME"".pub" ]] ;then 
-# 
-#echo "Creating priv key"
-#$(ssh-keygen -t rsa -N "" -f /.ssh/my.key)
-#fi
-#
-##$( cat /.ssh/"$FILE_NAME"".pub" >> /.ssh/authorized_keys ) 
-##$( rm -f /.ssh/"$FILE_NAME""
-#
-#ssh-copy-id -i "/.ssh/my.key.pub" 10.13.0.73
-#
+echo "Check out directory ~/.ssh  and comeback to script!"
+sleep 2 ; exit
+fi
+if [[ ! -f /root/.ssh/"$FILE_NAME" ]] &&  [[ !  -f /root/.ssh/"$FILE_NAME"".pub" ]] ;then 
+ 
+echo "Creating priv key"
+$(ssh-keygen -t rsa -b 2048 -N "" -f /root/.ssh/"$FILE_NAME")
+fi
 
+eval $(ssh-agent)
+$(ssh-add ~/.ssh/"$FILE_NAME")
+read -p "Write IP addr remote server: " ip_addr
+ssh-copy-id -i "/root/.ssh/""$FILE_NAME"".pub" "$ip_addr"  
+
+echo " "
+echo "1. Back to menu"
+read -p "Option: " back
+
+case $back in
+ 
+"1") menu ;;
+esac 
+}
+
+remote () {
+ssh -vvv $ip_addr
+
+}
+
+ 
 main() {
 
-check_existing
-
+menu
 }
 
 main
